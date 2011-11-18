e907fb23a89a32472ce7c979a8518c6d4f88158e
max ulidtko <ulidtko@gmail.com>
2011-11-18 02:09:30 +0200

    add main window

--- src/qtgui/main.cpp
+++ src/qtgui/main.cpp
@@ -1,7 +1,13 @@
 #include <QApplication>
 
+#include "MainWindow.h"
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    app.setApplicationName("Wave Simulation");
+
+    MainWindow w;
+    w.show();
 
     return app.exec();
 }
