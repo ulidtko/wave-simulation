977e6962dcd81123ff3e091f06f8b537be8f787d
max ulidtko <ulidtko@gmail.com>
2012-03-20 15:00:30 +0200

    tweak some constants after verifying the c

--- src/qtgui/MainWindow.cpp
+++ src/qtgui/MainWindow.cpp
@@ -3,18 +3,20 @@
 
 #include <QTimer>
 
+const float kMaxTps = 20.0; // simulation ticks per second
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
     , grid(new Grid(12.0, 8.0, 0.2))
-    , simulation(new Simulation(grid, 0.3, 10.0))
+    , simulation(new Simulation(grid, 1.0 / kMaxTps, 1.0))
 {
     ui->setupUi(this);
     ui->centralwidget->setGrid(grid);
 
     simulation->reset();
 
-    timer.setInterval(300);
+    timer.setInterval(1000 / kMaxTps);
     this->connect(&timer, SIGNAL(timeout()), SLOT(onTimer()));
     timer.start();
 }
