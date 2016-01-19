3c7a1140d087c53e6a9ec17ea06b727244292233
max ulidtko <ulidtko@gmail.com>
2016-01-19 07:27:18 +0200

    minor tweaks

--- src/qtgui/MyGLWidget.cpp
+++ src/qtgui/MyGLWidget.cpp
@@ -13,7 +13,7 @@
 
 void MyGLWidget::initializeGL() {
     glEnable(GL_TEXTURE_2D);
-    glClearColor(0.2, 0.2, 0.2, 0.0);
+    glClearColor(0.9, 0.9, 0.9, 0.0);
 
     auto img = QImage(":/android.jpg");
     texName = bindTexture(img, GL_TEXTURE_2D, GL_RGB,
@@ -26,7 +26,7 @@ void MyGLWidget::resizeGL(int w, int h) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     int m = std::min(w, h);
-    glOrtho(-0.8*w/m, +10.0*w/m, -0.8*h/m, +10.0*h/m, -1.0, 1.0);
+    glOrtho(-0.5*w/m, +9.0*w/m, -1.0*h/m, +8.5*h/m, -1.0, 1.0);
 
     glMatrixMode(GL_MODELVIEW);
 }
@@ -37,8 +37,8 @@ void MyGLWidget::paintGL() {
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
     glBindTexture(GL_TEXTURE_2D, texName);
 
-    ImageRenderer::render(*grid);
-    //MeshRenderer::render(*grid);
+    //ImageRenderer::render(*grid);
+    MeshRenderer::render(*grid);
 }
 
 tuple<float,float> window2scene(const QPoint& winCoord) {
